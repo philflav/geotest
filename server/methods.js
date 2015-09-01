578687e300f8bae856863ae50fa5126d13a114b7
Phil <pflavin@gmail.com>
2015-09-01 20:01:27 +0100

    Entirely new approach based on http://meteorcapture.com/how-to-create-a-reactive-google-map/

--- server/methods.js
+++
@@ -1,9 +0,0 @@
-Meteor.methods({
-    getGeoCode: function(search ,result) {
-        var geo = new GeoCoder();
-        result = geo.geocode(search);
-        console.log(result);
-
-        return result;
-    }
-});
